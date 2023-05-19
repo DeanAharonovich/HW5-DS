539c290b1b7f00157a34020fdf1607cfb663360e
Dean Aharonovich <dean.aha96@gmail.com>
2023-05-19 17:21:06 +0300

    Final version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,39 @@
 import java.util.Arrays;
 import java.util.Random;
 public class Main {
-    //amount of checks each algorithm does (NUMITER = 100 means each algorithm go 100 times)
-    private static final int NUMITER = 250;
-    private static final int[] inputSizes = {500000}; //the size the algorithms sorts
+    /** Options for testing */
+    private static int[] radixBases = { 2, (int)Math.pow(2,5), (int)Math.pow(2,10), (int)Math.pow(2,15),
+            (int)Math.pow(2,20), (int)Math.pow(2,25), (int)Math.pow(2,30) };
+    private static int[] radixRange = {(int)Math.pow(2,10), (int)Math.pow(2,20), (int)Math.pow(2,30)};
+    private static String[] inputArray = {"Random", "Increasing", "Decreasing"};
+
+
+    /** Configuration before each test */
+    private static final int NUMITER = 300; // Number of iterations for each algo
+    private static final int THRESHOLD = 8;
+    private static final int[] INPUT_SIZES = {1000000}; // Size of input array to be sorted
+    private static final String INPUT_ARRAY = inputArray[2]; // Input Array's order of values
+    private static final int RANGE = Integer.MAX_VALUE; // Range of values in the input array
+    private static final int RADIX_BASE = radixBases[3];
+
+
+    /** Main function for checking the run time of each algorithm. */
     public static void main(String[] args) {
         Sort sorter = new Sort();
-        Random random = new Random();
-        System.out.println();
-        System.out.println("NOTE: you might need to change the flags to see the exact checks you desire" +
-                "\nIn the beginning of the main function\n");
-        boolean flagRandom = true;
-        boolean flagIncreasing = false;
-        boolean flagDecreasing = false;
-        boolean flagRadix = false;
+        sorter.setNaiveSortThreshold(THRESHOLD);
 
-        if(flagRandom){
-        System.out.println("Results for random order array");
-        for (int n : inputSizes) {
+        for (int n : INPUT_SIZES) {
             double[][] durationList = new double[6][NUMITER];
 
             for (int k = 0; k < NUMITER; k++) {
                 Integer[] array = new Integer[n];
-                int[] radixArray = new int[n];
                 Integer[] copy;
-                int[] radixCopy;
 
-                //generate random array with Integer [0,MAX_Integer]
-                for (int i = 0; i < n; i++) {
-                    array[i] = random.nextInt();
-                    radixArray[i] = array[i].intValue();
-                }
+                // initialize array's value according to the desirable order.
+                initializeArrayByOrder(array, INPUT_ARRAY);
 
                 for (int i = 0; i < 6; i++) {
-                    copy = Arrays.copyOf(array, n);
-                    radixCopy = Arrays.copyOf(radixArray, n);
+                    copy = copyIntegerArray(array);
 
                     long startTime = System.currentTimeMillis();
                     switch (i) {
@@ -51,174 +50,78 @@ public class Main {
                             sorter.mergeSortIterative(copy);
                             break;
                         case 4:
-                            sorter.radixSort(radixCopy,10);
+                            sorter.radixSort(copyIntegerToInt(copy), RADIX_BASE);
                             break;
-                        case 5: //Java's sort
+                        case 5: // Java's sort
                             Arrays.sort(copy);
                             break;
                     }
                     long endTime = System.currentTimeMillis();
-                    //store the runtime of the current run to the current algo type
+
+                    // stores the algorithm's runtime in the table
                     durationList[i][k] = ((double) endTime - startTime);
                 }
             }
 
             // Prints average durations for each algorithm and input size
             System.out.println("Input Size: " + n);
+            System.out.println("Input array: " + INPUT_ARRAY);
             System.out.println("Algorithm\t|\tAverage Duration (ms)\t|\tStandard Deviation");
             for (int i = 0; i < 6; i++) {
                 printResults(durationList[i], i);
             }
             System.out.println();
         }
+    }
+
+    /** Helper function for initializing the input array.
+     * @param array = the input array to initialize.
+     * @param orderType = the order type for the input array. 3 possibilities:
+     *                  "Random", "sorted increasing", or "sorted decreasing" .*/
+    private static void initializeArrayByOrder(Integer[] array, String orderType) {
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            // Generate random Integers to the array form 0 to upper-bound parameter.
+            // For radix analyze - enter a value from radixRange array.
+            array[i] = random.nextInt(RANGE) & Integer.MAX_VALUE;
         }
 
-        if(flagIncreasing) {
-            System.out.println("Results for Increasing order array");
-            for (int n : inputSizes) {
-                double[][] durationList = new double[6][NUMITER];
-
-                for (int k = 0; k < NUMITER; k++) {
-                    Integer[] array = new Integer[n];
-                    Integer[] copy;
-
-                    //generate random array with Integer [0,MAX_Integer]
-                    for (int i = 0; i < n; i++) {
-                        array[i] = random.nextInt(Integer.MAX_VALUE);
-                    }
-                    Arrays.sort(array);
-
-                /* 1) clone the random Array each algo measure
-                   2) use the i'th algorithem on the clone
-                   3) save the result
-                 */
-                    for (int i = 0; i < 6; i++) {
-                        copy = copyIntegerArray(array);
-                        long startTime = System.currentTimeMillis();
-                        switch (i) {
-                            case 0:
-                                sorter.quickSortClass(copy);
-                                break;
-                            case 1:
-                                sorter.quickSortRecitation(copy);
-                                break;
-                            case 2:
-                                sorter.mergeSortRecursive(copy);
-                                break;
-                            case 3:
-                                sorter.mergeSortIterative(copy);
-                                break;
-                            case 4://this one gets different input since it works specificly with int[] type
-                                sorter.radixSort(copyIntegerTOint(copy), 3);
-                                break;
-                            case 5://naive Sort
-                                Arrays.sort(copy);
-                                break;
-                        }
-                        long endTime = System.currentTimeMillis();
-                        //store the runtime of the current run to the current algo type
-                        durationList[i][k] = ((double) endTime - startTime);
-                    }
-                }
-
-                // Prints average durations for each algorithm and input size
-                System.out.println("Input Size: " + n);
-                System.out.println("Algorithm\t|\tAverage Duration (ms)\t|\tStandard Deviation");
-                for (int i = 0; i < 6; i++) {
-                    printResults(durationList[i], i);
-                }
-                System.out.println();
-            }
+        if (orderType.equals(inputArray[0])){
+            return;
         }
 
-        if(flagDecreasing) {
-            System.out.println("Results for Decreasing order array");
-            for (int n : inputSizes) {
-                double[][] durationList = new double[6][NUMITER];
+        if (orderType.equals(inputArray[1])){
+            Arrays.sort(array);
+        }
 
-                for (int k = 0; k < NUMITER; k++) {
-                    Integer[] array = new Integer[n];
-                    Integer[] copy;
-                    //generate random array with Integer [0,MAX_Integer]
-                    for (int i = 0; i < n; i++) {
-                        array[i] = random.nextInt(Integer.MAX_VALUE);
-                    }
-                    Arrays.sort(array);
-                    reverseArray(array);
+        if (orderType.equals(inputArray[2])){
+            Arrays.sort(array);
+            int left = 0;
+            int right = array.length - 1;
 
-                /* 1) clone the random Array each algo measure
-                   2) use the i'th algorithem on the clone
-                   3) save the result
-                 */
-                    for (int i = 0; i < 6; i++) {
-                        copy = copyIntegerArray(array);
-                        long startTime = System.currentTimeMillis();
-                        switch (i) {
-                            case 0:
-                                sorter.quickSortClass(copy);
-                                break;
-                            case 1:
-                                sorter.quickSortRecitation(copy);
-                                break;
-                            case 2:
-                                sorter.mergeSortRecursive(copy);
-                                break;
-                            case 3:
-                                sorter.mergeSortIterative(copy);
-                                break;
-                            case 4://this one gets different input since it works specificly with int[] type
-                                sorter.radixSort(copyIntegerTOint(copy), 3);
-                                break;
-                            case 5://naive Sort
-                                Arrays.sort(copy);
-                                break;
-                        }
-                        long endTime = System.currentTimeMillis();
-                        //store the runtime of the current run to the current algo type
-                        durationList[i][k] = ((double) endTime - startTime);
-                    }
-                }
+            while (left < right) {
+                // Swap elements at left and right indices
+                int temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
 
-                // Prints average durations for each algorithm and input size
-                System.out.println("Input Size: " + n);
-                System.out.println("Algorithm\t|\tAverage Duration (ms)\t|\tStandard Deviation");
-                for (int i = 0; i < 6; i++) {
-                    printResults(durationList[i], i);
-                }
-                System.out.println();
+                // Move left and right indices towards the center
+                left++;
+                right--;
             }
         }
     }
 
-    public static void printResults(double[] arr,int i){
+    /** Helper function for printing the run time results. */
+    private static void printResults(double[] arr,int i){
         double averageDuration = calculateAverage(arr);
         double standardDeviation = calcStandardDeviation(arr);
-        System.out.println(getAlgorithmName(i) + "\t\t" + averageDuration + "\t\t" + standardDeviation);
+        System.out.printf(getAlgorithmName(i) + "\t\t" + "%.2f" + "\t\t" + "%.2f%n", averageDuration, standardDeviation);
     }
 
-    /**
-     * Reverses the order of elements in an integer array.
-     * @param array The array to be reversed.
-     */
-    public static void reverseArray(Integer[] array) {
-        int left = 0;
-        int right = array.length - 1;
-
-        while (left < right) {
-            // Swap elements at left and right indices
-            int temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
-
-            // Move left and right indices towards the center
-            left++;
-            right--;
-        }
-    }
-
-
-    //regular copying function
-    public static Integer[] copyIntegerArray(Integer[] a) {
+    /** Helper function for creating a copy of an Integer array. */
+    private static Integer[] copyIntegerArray(Integer[] a) {
         Integer[] copy = new Integer[a.length];
         for (int i = 0; i < a.length; i++) {
             copy[i] = a[i];
@@ -226,12 +129,10 @@ public class Main {
         return copy;
     }
 
-    /**
-     *
-     * @param a is the original array we randomed
-     * @return a clone array of type intp[] compatible for radix-sort
-     */
-    public static int[] copyIntegerTOint(Integer[] a) {
+    /** Helper function for copying an Integer array to an int array.
+     * @param a is the original array we generated.
+     * @return a clone array of type int[] compatible for radix-sort. */
+    private static int[] copyIntegerToInt(Integer[] a) {
         int[] copy = new int[a.length];
         for (int i = 0; i < a.length; i++) {
             copy[i] = a[i];
@@ -240,11 +141,9 @@ public class Main {
     }
 
 
-    /**
-     * Runs over all the results of the experemint of the given algo
-     * @param array is subarray of a 2-dim array
-     * @return Avg time to sort
-     */
+    /** Runs over all the results of the experiment of the given algo and calculate their average.
+     * @param array is subarray of a 2-dim array.
+     * @return Avg running time of algorithm. */
     private static double calculateAverage(double[] array) {
         double sum = 0;
         for (double value : array) {
@@ -253,13 +152,12 @@ public class Main {
         return sum / array.length;
     }
 
-    /**
-     * Calculates the standard deviation of a given array of measurements values.
-     * @param array The input array of double values.
-     * @return The standard deviation of the input array.
-     */
-    public static double calcStandardDeviation(double[] array) {
+    /** Calculates the standard deviation of a given array of measurements values.
+     * @param array = The input array of double values.
+     * @return The standard deviation the values. */
+    private static double calcStandardDeviation(double[] array) {
         double avg = calculateAverage(array);
+
         //calculation according to the given formula in the hw5 doc.
         double squaredDiffSum = 0; //represents the Sigma
         for (double current : array) {
@@ -270,6 +168,7 @@ public class Main {
         return standardDeviation;
     }
 
+    /** Helper function for the algorithm name for printing the results. */
     private static String getAlgorithmName(int algorithm) {
         switch (algorithm) {
             case 0:
--- src/main/java/Sort.java
+++ src/main/java/Sort.java
@@ -1,12 +1,23 @@
 import java.lang.reflect.Array;
 
 public class Sort<T extends Comparable<T>> {
-    private int threshold = 3; //a threshold to determine if to use the naive sort or the algorithm
-    /** Lecture quick sort's subroutines - Main function and private helper functions */
+    private int threshold = 3; // A threshold to determine at which point to activate the naive sort algo
+
+
+    /** Lecture quick sort's algorithm.
+     * Sort's the array in an increasing order
+     * @param array = the array to be sorted */
     public void quickSortClass(T[] array){
         quickSort(array, 0, array.length - 1, "Class");
     }
 
+    /** QuickSort method for recursive calls.
+     * @param array - The array to be sorted.
+     * @param p = The starting index of the subarray to be sorted.
+     * @param r = The ending index of the subarray to be sorted.
+     * @param type = A string indicating the type of partition to be performed.
+     *             "Class" for the partition from the lecture
+     *             "Recitation" for the partition from the recitation .*/
     private void quickSort(T[] array, int p, int r, String type){
         int pivotIndex;
         if (r - p > threshold - 1){
@@ -28,10 +39,15 @@ public class Sort<T extends Comparable<T>> {
         }
     }
 
+    /** Partition as taught in the lecture.
+     * @param array = The array to find the pivot from.
+     * @param p = The starting index of the subarray to find the pivot from.
+     * @param r = The ending index of the subarray to find the pivot from.
+     * @return = pivot's index. */
     private int partitionClass(T[] array, int p, int r){
         T pivot = array[r];
-            int rightIndex = r, leftIndex = p - 1;
-        T temp;
+        int rightIndex = r, leftIndex = p - 1;
+        T temp; // for swapping
 
         while (true){
             do {
@@ -42,13 +58,13 @@ public class Sort<T extends Comparable<T>> {
                 leftIndex ++;
             } while (leftIndex <= r && array[leftIndex].compareTo(pivot) < 0);
 
-            if (leftIndex < rightIndex){ //if found 2 elements to swap
-                temp = array[leftIndex]; //Swap them
+            if (leftIndex < rightIndex){ // found 2 elements to swap
+                temp = array[leftIndex]; // Swap them
                 array[leftIndex] = array[rightIndex];
                 array[rightIndex] = temp;
             }
 
-            else{ //leftIndex and RightIndex crossed,
+            else { // leftIndex and RightIndex crossed
                 // swap pivot with R+1
                 temp = array[r];
                 array[r] = array[rightIndex+1];
@@ -58,15 +74,11 @@ public class Sort<T extends Comparable<T>> {
         }
     }
 
-    public void bubbleSort(T[] array) {
-        int n = array.length;
-        for (int i = 0; i < n - 1; i++)
-            for (int j = 0; j < n - i - 1; j++)
-                if (array[j].compareTo(array[j + 1]) > 0 ) {
-                    swap(array,j,j+1);
-                }
-    }
-
+    /** Naive sorting algorithm - Bubble sort.
+     * Sorts the array in O(n^2).
+     * @param array = The array to find the pivot from.
+     * @param start = The starting index of the subarray to find the pivot from.
+     * @param end = The ending index of the subarray to find the pivot from. */
     public void bubbleSort(T[] array, int start, int end) {
         for (int i = start; i <= end; i++) {
             for (int j = start; j < end; j++) {
@@ -77,10 +89,18 @@ public class Sort<T extends Comparable<T>> {
         }
     }
 
+    /** Recitation quick sort's algorithm.
+     * Sort's the array in an increasing order
+     * @param array = the array to be sorted */
     public void quickSortRecitation(T[] array){
         quickSort(array, 0, array.length - 1, "Recitation");
     }
 
+    /** Partition as taught in the Recitation.
+     * @param array = The array to find the pivot from.
+     * @param p = The starting index of the subarray to find the pivot from.
+     * @param r = The ending index of the subarray to find the pivot from.
+     * @return = pivot's index. */
     private int partitionRecitation(T[] array, int p, int r){
         T pivot = array[r];
         int left = p - 1;
@@ -102,6 +122,12 @@ public class Sort<T extends Comparable<T>> {
         return left + 1;
     }
 
+    /** The radixSort algorithm.
+     * Sorts the array in ascending order based on their digits,
+     * considering each digit position from the least significant digit to the most significant digit.
+     * @param array = An array of integers to be sorted.
+     * @param base = Representing the base value of the numbers in the array.
+     *           For example, if base is 10, it indicates a decimal number system. */
     public static void radixSort(int[] array, int base){
         // Find the number with the max digits
         int maxNumber = 0;
@@ -112,27 +138,38 @@ public class Sort<T extends Comparable<T>> {
             }
         }
 
-        for (int digit = 1; maxNumber / digit > 0; digit *= base) {
+        for (int digit = 1; digit != 0 && maxNumber / digit > 0; digit *= base) {
             countingSort(array, base, digit);
         }
     }
 
+    /** The countingSort algorithm.
+     * Used to sort an array of positive integers based on a specific digit position.
+     * It is a stable sorting algorithm that counts the occurrences of each element
+     * and determines their correct positions in the sorted output.
+     * @param arrA = An array of integers to be sorted.
+     * @param base = Representing the base value of the numbers in the array.
+     * @param digit = The digit position based on which the sorting will be performed. */
     private static void countingSort(int[] arrA, int base, int digit){
         int[] arrB = new int[arrA.length];
-        int[] arrC = new int[base];
+        int[] arrC = new int[base]; // array for counting the number of appearance.
 
-        for (int i = 0; i < base; i++){ //we can delete that, by default java puts 0
+        // initialize array C with 0
+        for (int i = 0; i < base; i++) {
             arrC[i] = 0;
         }
 
+        // counting the number of appearance of each digit
         for (int j = 1; j < arrA.length; j++){
             arrC[(arrA[j] / digit) % base] ++;
         }
 
+        // adding the number of appearance of the previous digit for stability
         for (int i = 1; i < base; i++){
             arrC[i] += arrC[i-1];
         }
 
+        // sorts the array's elements in array B with the help of array C
         for (int j = arrA.length - 1; j >= 0; j--){
             arrB[arrC[(arrA[j] / digit) % base]] = arrA[j];
             arrC[(arrA[j] / digit) % base] --;
@@ -144,24 +181,37 @@ public class Sort<T extends Comparable<T>> {
         }
     }
 
+    /** Merge sort algorithm - Recursive implementation.
+     * Sorts the array in an increasing order.
+     * @param array = The array to be sorted. */
     public void mergeSortRecursive(T[] array){
         mergeSortRecursive(array, 0, array.length-1);
     }
 
-    public void mergeSortRecursive(T[] arr, int left, int right){
+    /** Merge sort method for recursive calls.
+     * @param array = The array to be sorted.
+     * @param left = The starting index of the subarray to be sorted.
+     * @param right = The ending index of the subarray to be sorted. */
+    public void mergeSortRecursive(T[] array, int left, int right){
         if (left < right) {
             if (right - left + 1 <= threshold) {
-                bubbleSort(arr, left, right);
+                bubbleSort(array, left, right);
             } else {
                 int mid = (left + right) / 2;
-                mergeSortRecursive(arr, left, mid);
-                mergeSortRecursive(arr, mid + 1, right);
-                merge(arr, left, mid, right);
+                mergeSortRecursive(array, left, mid);
+                mergeSortRecursive(array, mid + 1, right);
+                merge(array, left, mid, right);
             }
         }
     }
 
-    private void merge(T[] arr, int left, int mid, int right) {
+    /** The merge method is a helper function used in the merge sort
+     *  algorithm to merge two sorted sub-arrays into a single sorted array.
+     * @param array = An array of objects to be sorted.
+     * @param left = The starting index of the first sub-array.
+     * @param mid = The index marking the end of the first sub-array and the start of the second subarray.
+     * @param right = The index marking the end of the second sub-array. */
+    private void merge(T[] array, int left, int mid, int right) {
         //size of sub-arrays
         int n1 = mid - left + 1;
         int n2 = right - mid;
@@ -169,35 +219,40 @@ public class Sort<T extends Comparable<T>> {
         T[] rightAr = (T[]) Array.newInstance(Comparable.class, n2);
 
         for(int i=0;i<n1;i++){
-            leftAr[i]=arr[left+i];
+            leftAr[i]=array[left+i];
         }
+
         for(int i=0;i<n2;i++){
-            rightAr[i]=arr[mid + 1 + i];
+            rightAr[i]=array[mid + 1 + i];
         }
 
         int i = 0, j = 0, k = left;
         while (i < n1 && j < n2) {
             if (leftAr[i].compareTo(rightAr[j]) <= 0) {
-                arr[k++] = leftAr[i++];
+                array[k++] = leftAr[i++];
             } else {
-                arr[k++] = rightAr[j++];
+                array[k++] = rightAr[j++];
             }
         }
+
         while (i < n1) {
-            arr[k++] = leftAr[i++];
+            array[k++] = leftAr[i++];
         }
 
         while (j < n2) {
-            arr[k++] = rightAr[j++];
+            array[k++] = rightAr[j++];
         }
     }
 
+    /** Merge sort algorithm - Iterative implementation.
+     * Sorts the array in an increasing order.
+     * @param array = The array to be sorted. */
     public void mergeSortIterative(T[] array) {
         int n = array.length;
-        if(n<=threshold){
-            bubbleSort(array);
+        if (n <= threshold){
+            bubbleSort(array,0, array.length - 1); // Naive sorting algo
         }
-        else{
+        else {
             // Divide the array into sub-arrays of size 1, then merge them pairwise
             for (int size = 1; size < n; size *= 2) {
                 for (int left = 0; left < n - size; left += 2 * size) {
@@ -208,12 +263,15 @@ public class Sort<T extends Comparable<T>> {
             }
         }
     }
+
+    /** Helper function for swapping two array's elements. */
     private void swap(T[] array, int i, int j) {
         T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
     }
 
+    /** Setter method for the threshold. */
     public void setNaiveSortThreshold(int threshold){
         this.threshold = threshold;
     }
